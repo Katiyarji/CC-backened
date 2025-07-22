cc27681df2758a99429d3fd45da83cfaf22a8fac
Katiyarji <richa.katiyar1086@gmail.com>
2025-07-22 08:17:31 -0700

    register controller and auth middleware

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 import connectDB from './src/db/db.js'
-import {app,PORT} from './src/app.js'
 import dotenv from 'dotenv'
-dotenv.config();
+import userRoute from "./src/routes/user.routes.js"
+dotenv.config({
+  path: './.env'
+});
+
+connectDB();
+const app = express();
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"))
+app.use(cookieParser);
 
 
-connectDB().
-then(
-    app.listen( PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})).
-catch("error",(error)=>{
-  console.log("MONGODB connection failed !!");
-  
+app.use("/api/v1/users", userRoute)
+app.get("/", (req, res) => {
+  res.send("hello world")
 })
 
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+})
 
 
--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
-import express from 'express'
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
 
-const app = express();
-const PORT=process.env.PORT||3000
-app.use(cors({
-  origin:process.env.CORS_ORIGIN,
-  credentials:true
-}))
 
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}));
-app.use(express.static("public"))
-app.use(cookieParser);
+
 
 //routes import
-import userRoute from './routes/user.routes.js'
+
 
 
 //routes declaration
-app.use("/api/v1/users",userRoute)
+
 
 
 
